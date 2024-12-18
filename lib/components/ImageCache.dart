22d2e6aecd8d21ea1bacd09e2e234d9348e17308
vineet <vineetkumarsingh341@gmail.com>
2024-12-18 12:58:12 +0530

    cached image locally

---
+++ lib/components/ImageCache.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+
+Widget buildCachedNetworkImage({
+  required String imageUrl,
+  int cacheDurationInDays=1,
+  double? width,
+  double? height,
+  BoxFit fit = BoxFit.cover,
+}) {
+  return CachedNetworkImage(
+    imageUrl: imageUrl,
+    placeholder: (context, url) => const CircularProgressIndicator(),
+    errorWidget: (context, url, error) => const Icon(Icons.error),
+    cacheManager: CacheManager(
+      Config(
+        imageUrl.hashCode.toString(), // Unique key for caching
+        stalePeriod: Duration(days: cacheDurationInDays), // Cache duration
+      ),
+    ),
+    width: width,
+    height: height,
+    fit: fit,
+  );
+}
--- lib/components/cairosal.dart
+++ lib/components/cairosal.dart
@@ -1,9 +1,9 @@
+import 'package:ecommerce/components/ImageCache.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class ImageCarousel extends StatefulWidget {
-
-  const ImageCarousel({super.key,required this.imageList});
+  const ImageCarousel({super.key, required this.imageList});
 
   final List<String> imageList;
 
@@ -12,46 +12,44 @@ class ImageCarousel extends StatefulWidget {
 }
 
 class _ImageCarouselState extends State<ImageCarousel> {
-  // List of image paths or URLs
-
-
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-          options: CarouselOptions(
-            height: 200.0, // Adjust height as needed
-            autoPlay: true, // Auto-scroll the carousel
-            enlargeCenterPage: true, // Enlarge current image
-            aspectRatio: 2/1,
-            viewportFraction: 0.6, // Fraction of the page to show
-            autoPlayInterval: const Duration(seconds: 3), // Interval between auto-scroll
-          ),
-          items: widget.imageList.map((imagePath) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  // margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 6,
-                        offset: const Offset(0, 4), // Shadow position
-                      ),
-                    ],
+      options: CarouselOptions(
+        height: 200.0, // Adjust height as needed
+        autoPlay: true, // Auto-scroll the carousel
+        enlargeCenterPage: true, // Enlarge current image
+        aspectRatio: 2 / 1,
+        viewportFraction: 0.6, // Fraction of the page to show
+        autoPlayInterval: const Duration(seconds: 3), // Interval between auto-scroll
+      ),
+      items: widget.imageList.map((imagePath) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 6,
+                    offset: const Offset(0, 4), // Shadow position
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15.0),
-                    child: Image.network(
-                      imagePath,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                );
-              },
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+
+                child: buildCachedNetworkImage(
+                  imageUrl: imagePath,
+                  cacheDurationInDays: 1,
+                  fit: BoxFit.cover,
+                ),
+              ),
             );
-          }).toList(),
+          },
+        );
+      }).toList(),
     );
   }
 }

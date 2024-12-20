dbd69118fd905ab70acab2a0a84bb358049e387d
vineet <vineetkumarsingh341@gmail.com>
2024-12-20 13:41:01 +0530

    optimise the db calls

--- lib/screen/Product_list.dart
+++ lib/screen/Product_list.dart
@@ -1,78 +1,24 @@
 import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
+
 import '../components/Product_card.dart';
-import '../data/model/product.dart';
-import '../services/api_service.dart';
-import '../services/databaseServices.dart';
+import '../providers/product.dart';
 
-class ProductPage extends StatefulWidget {
-  const ProductPage({super.key, required this.endpoint});
-
-  final String endpoint;
-
-  @override
-  State<ProductPage> createState() => _ProductPageState();
-}
-
-class _ProductPageState extends State<ProductPage> {
-  List<Product> productData = [];
-  final DatabaseService _databaseService = DatabaseService();
-
-  // Fetch data from the API using the endpoint
-  Future<void> fetchDataFromApi() async {
-    try {
-      final apiService = ApiService(baseUrl: 'https://fakestoreapi.com');
-      final jsonList = await apiService.request(endpoint: widget.endpoint, method: 'GET');
-
-      Logger().i('API Response: $jsonList');
-
-      final fetchedProducts = jsonList.map<dynamic>((json) {
-        try {
-          return Product.fromJson(json);
-        } catch (e) {
-          Logger().e('Error parsing product JSON: $e');
-          return null; // Skip invalid products
-        }
-      }).whereType<Product>().toList();
-
-      setState(() {
-        productData = fetchedProducts;
-      });
-
-      // Save fetched products to the database
-      for (var product in fetchedProducts) {
-        await _databaseService.insertOrUpdateProduct(product);
-      }
-    } catch (e) {
-      Logger().e('Error fetching data: $e');
-    }
-  }
-
-  // Fetch data from the local Sembast database
-  Future<void> fetchDataFromDb() async {
-    final productsFromDb = await _databaseService.getAllProducts();
-    setState(() {
-      productData = productsFromDb;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Initialize database and fetch data
-    _databaseService.init().then((_) {
-      fetchDataFromDb().then((_) {
-        if (productData.isEmpty) {
-          fetchDataFromApi();
-        }
-      });
-    });
-  }
-
+class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productProvider = Provider.of<ProductProvider>(context);
+
+    if (productProvider.products.isEmpty && !productProvider.isData) {
+      productProvider.getProducts();
+    }
+
+    final productData = productProvider.products;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: productData.isEmpty

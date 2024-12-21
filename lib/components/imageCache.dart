feacd61f0ab54dfb425ebaf888b88b366199f4f4
vineet <vineetkumarsingh341@gmail.com>
2024-12-21 23:03:59 +0530

    reduce the size of caching imageloader

--- lib/components/imageCache.dart
+++ lib/components/imageCache.dart
@@ -1,19 +1,26 @@
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-
 Widget buildCachedNetworkImage({
   required String imageUrl,
-  int cacheDurationInDays=1,
+  int cacheDurationInDays = 1,
   double? width,
   double? height,
   BoxFit fit = BoxFit.cover,
 }) {
   return CachedNetworkImage(
     imageUrl: imageUrl,
-    placeholder: (context, url) => const CircularProgressIndicator(),
+    placeholder: (context, url) => Center(
+      child: SizedBox(
+        width: 24, 
+        height: 24,
+        child: CircularProgressIndicator(
+          strokeWidth: 2, 
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.grey), 
+        ),
+      ),
+    ),
     errorWidget: (context, url, error) => const Icon(Icons.error),
     cacheManager: CacheManager(
       Config(
